e9c4bf1f23e98fdd790d37766b206cb31636086e
danny puckett <danny puckett>
2016-09-06 14:51:37 -0400

    project, homework coming thru...

---
+++ prj/project0/project0.java
@@ -0,0 +1,6 @@
+ public class project0 { 
+   public static void main(String[] args) { 
+     // Display message Welcome to Java! on the console 
+     System.out.println("Hello World!"); 
+   } 
+ }
